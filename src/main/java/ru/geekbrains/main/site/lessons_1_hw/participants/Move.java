a739aaeab3122a5bd56fb124f03fa574d1fcc056
lpk7 <lpk_87@bk.ru>
2020-05-27 01:26:44 +0300

    Удалены файлы прошлого урока

--- src/main/java/ru/geekbrains/main/site/lessons_1_hw/participants/Move.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.main.site.lessons_1_hw.participants;
-
-import ru.geekbrains.main.site.lessons_1_hw.оbstacles.Barrier;
-
-public interface Move {
-    void info();
-    boolean jump(Barrier wall);
-    boolean run(Barrier treadmill);
-}
--- src/main/java/ru/geekbrains/main/site/lessons_1_hw/оbstacles/Barrier.java
+++
@@ -1,5 +0,0 @@
-package ru.geekbrains.main.site.lessons_1_hw.оbstacles;
-
-public interface Barrier {
-    float get();
-}
